a888830fe8767ab413758b9b86f4fc41783d2f76
Diego Garcia Linares <garcialinaresdiego@gmail.com>
2024-10-11 21:17:11 +0200

    feat: Login y cambios en css

---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,63 @@
+import { JsonPipe, CommonModule } from '@angular/common';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Component, signal } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterOutlet } from '@angular/router';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [MatInputModule,
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    RouterOutlet,
+    HttpClientModule
+  ],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+  form = signal<FormGroup>(
+    new FormGroup(
+      {
+        password: new FormControl('', [Validators.required]),
+        username: new FormControl('', [Validators.required]),
+      }, { updateOn: 'submit' }
+    )
+  );
+
+  errorMessage: string | null = null;
+
+  constructor(private http: HttpClient) { }
+
+  onSubmit() {
+    console.log('Form submitted');
+    this.errorMessage = null; // Reinicia el mensaje de error al enviar el formulario
+
+    if (this.form().valid) {
+      console.log('Form valid');
+      this.http.post('http://localhost:3000/api/login', this.form().value)
+        .pipe(
+          catchError((error) => {
+            if (error.status === 400) {
+              this.errorMessage = error.error.message;
+            } else {
+              this.errorMessage = 'An unexpected error occurred. Please try again later.';
+            }
+            return throwError(error);
+          })
+        )
+        .subscribe(response => {
+          console.log('Registration successful', response);
+        }, error => {
+          console.error('Registration failed', error);
+        });
+    }
+  }
+}
